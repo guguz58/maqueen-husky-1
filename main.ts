a52d570fdbcbbee7222ea3d24eefaf35212b8351
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2025-01-16 15:53:44 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,26 @@
-// Set MOVE variable to TRUE - this "activates" the display loop in the FOREVER block
-// 
-// Display TICK icon to show it has been activated
-input.onButtonPressed(Button.A, function () {
-    Move = true
-    basic.showIcon(IconNames.Yes)
-    basic.pause(1000)
-})
-// Set MOVE variable to FALSE to "deactivate" the display part of the FOREVER block
-// 
-// Also turn off both Maqueen LED
-input.onButtonPressed(Button.B, function () {
-    Move = false
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-})
+function cizgivarsa () {
+    pos = huskylens.readeArrow(1, Content2.xOrigin)
+    if (pos >= 150 && pos <= 170) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, maxspeed)
+    } else if (pos < 150) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, maxspeed)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, minspeed)
+    } else if (pos > 170) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, maxspeed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, minspeed)
+    }
+    lastpos = pos
+}
+function cizgiyoksa () {
+    if (lastpos < 150) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, maxspeed)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, minspeed)
+    }
+    if (lastpos > 170) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, maxspeed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, minspeed)
+    }
+}
 // Start Communication between HuskyLens & Micro:Bit
 // 
 // Clear Micro:Bit Screen
@@ -24,9 +31,14 @@ input.onButtonPressed(Button.B, function () {
 // 
 // If you don't get this far check the connections between HUSKYLENS & Micro:Bit
 // Also Check correct Protocol is set on HUSKYLENS as well as make sure the HUSKYLENS is getting adequate power
-let Move = false
+let lastpos = 0
+let pos = 0
+let minspeed = 0
+let maxspeed = 0
 huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
+huskylens.initMode(protocolAlgorithm.ALGORITHM_LINE_TRACKING)
+maxspeed = 50
+minspeed = 10
 // Request latest result from HUSKYLENS at start of FOREVER so that each time it repeats it has the latest result
 // 
 // If MOVE variable is set to TRUE - show the recognised ID# from the HUSKYLENS & display its number / light LED based on the result
@@ -34,28 +46,13 @@ huskylens.initMode(protocolAlgorithm.ALGORITHM_TAG_RECOGNITION)
 // If MOVE is set to FALSE turn off BOTH MaQueen LED & Display X on MicroBit
 basic.forever(function () {
     huskylens.request()
-    if (Move) {
-        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showNumber(1)
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showNumber(2)
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-        } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-            basic.showNumber(3)
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    if (huskylens.isLearned(1)) {
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultArrow)) {
+            cizgivarsa()
         } else {
-            basic.clearScreen()
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            cizgiyoksa()
         }
     } else {
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.motorStop(maqueen.Motors.All)
-        basic.showIcon(IconNames.No)
     }
 })
